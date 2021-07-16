ba1529e175b7e0b5390a2d0f1a34ca24d1272e9d
Hackerl <490021209@qq.com>
2021-07-16 23:23:43 +0800

    Add#syscall

--- z_syscall.c
+++ z_syscall.c
@@ -2,18 +2,56 @@
 #include "do_syscall.h"
 #include <syscall.h>
 
-void z_exit(int status) {
-    do_syscall(SYS_exit, status);
+static int errno;
+
+int *z_errno_location() {
+    return &errno;
 }
 
-ssize_t z_write(int fd, const void *buf, size_t count) {
-    return (ssize_t)do_syscall(SYS_write, fd, buf, count);
+static long int check_error(long int rc) {
+    if (rc < 0 && rc > -4096) {
+        errno = (int)-rc;
+        rc = -1;
+    }
+
+    return rc;
 }
+
+#define SYSCALL(name, ...) check_error(do_syscall(SYS_##name, __VA_ARGS__))
+
+#define DEFINE_SYSCALL1(name, ret, t1, a1)                                  \
+ret z_##name(t1 a1) {                                                       \
+	return (ret)SYSCALL(name, a1);                                          \
+}
+
+#define DEFINE_SYSCALL2(name, ret, t1, a1, t2, a2)                          \
+ret z_##name(t1 a1, t2 a2) {                                                \
+    return (ret)SYSCALL(name, a1, a2);                                      \
+}
+
+#define DEFINE_SYSCALL3(name, ret, t1, a1, t2, a2, t3, a3)                  \
+ret z_##name(t1 a1, t2 a2, t3 a3) {                                         \
+	return (ret)SYSCALL(name, a1, a2, a3);                                  \
+}
+
+DEFINE_SYSCALL1(exit, void, int, status)
+
+DEFINE_SYSCALL2(open, int, const char *, filename, int, flags)
+DEFINE_SYSCALL1(close, int, int, fd)
+DEFINE_SYSCALL3(lseek, int, int, fd, off_t, off, int, whence)
+DEFINE_SYSCALL3(read, ssize_t, int, fd, void *, buf, size_t, count)
+DEFINE_SYSCALL3(write, ssize_t, int, fd, const void *, buf, size_t, count)
+
+DEFINE_SYSCALL2(munmap, int, void *, addr, size_t, length)
+DEFINE_SYSCALL3(mprotect, int, void *, addr, size_t, length, int, prot)
 
 void *z_mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset) {
-    return (void *)do_syscall(SYS_mmap, addr, length, prot, flags, fd, offset);
-}
-
-int z_munmap(void *addr, size_t length) {
-    return (int)do_syscall(SYS_munmap, addr, length);
+#ifdef __i386__
+    /* i386 has old_mmap and mmap2, old_map is a legacy single arg
+	 * function, use mmap2 but it needs offset in page units. */
+	offset = (unsigned long long)offset >> 12;
+	return (void *)SYSCALL(mmap2, addr, length, prot, flags, fd, offset);
+#else
+    return (void *)SYSCALL(mmap, addr, length, prot, flags, fd, offset);
+#endif
 }
--- z_syscall.h
+++ z_syscall.h
@@ -4,11 +4,20 @@
 #include <stddef.h>
 #include <sys/types.h>
 
+#define z_errno	*z_errno_location()
+
+int	*z_errno_location();
+
 void z_exit(int status);
 
+int z_open(const char *pathname, int flags);
+int	z_close(int fd);
+int	z_lseek(int fd, off_t offset, int whence);
+ssize_t	z_read(int fd, void *buf, size_t count);
 ssize_t z_write(int fd, const void *buf, size_t count);
 
 void *z_mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
 int z_munmap(void *addr, size_t length);
+int	z_mprotect(void *addr, size_t length, int prot);
 
 #endif //C_RUNTIME_Z_SYSCALL_H
