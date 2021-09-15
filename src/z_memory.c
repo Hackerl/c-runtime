3ec3d1990cc500a65eab3e03b67e4a6082e1d0a8
Hackerl <490021209@qq.com>
2021-09-15 23:34:16 +0800

    Add#circular buffer

--- src/z_memory.c
+++ src/z_memory.c
@@ -1,66 +1,103 @@
 #include <z_memory.h>
 #include <z_syscall.h>
 #include <z_std.h>
+#include <z_sync.h>
 #include <sys/mman.h>
 
 #ifndef PAGE_SIZE
 #define PAGE_SIZE 0x1000
 #endif
 
+#define CRT_PTR_RAW(ptr)        ((unsigned long *)(ptr) - 2)
 #define CRT_PTR_USER(ptr)       ((unsigned long *)(ptr) + 2)
 #define CRT_SIZE_USER(ptr)      (*((unsigned long *)(ptr) - 2))
 #define CRT_SIZE_ALLOC(ptr)     (*((unsigned long *)(ptr) - 1))
 #define CRT_SIZE_HDR            (2 * sizeof(unsigned long))
 
+#define HEAP_CACHE_COUNT        100
+#define HEAP_CACHE_LOWER        0x1000
+#define HEAP_CACHE_UPPER        0x10000
+#define HEAP_CACHE_RETRY        5
+
+static void *cache_buffer[HEAP_CACHE_COUNT] = {};
+static z_circular_buffer_state_t cache_state[HEAP_CACHE_COUNT] = {};
+
+static z_circular_buffer_t cache = {
+        0,
+        0,
+        sizeof(void *),
+        HEAP_CACHE_COUNT,
+        cache_buffer,
+        cache_state
+};
+
 void z_free(void *ptr) {
     if (!ptr)
         return;
 
-    z_munmap((char *)(ptr) - CRT_SIZE_HDR, CRT_SIZE_ALLOC(ptr));
+    if (CRT_SIZE_ALLOC(ptr) >= HEAP_CACHE_LOWER && CRT_SIZE_ALLOC(ptr) <= HEAP_CACHE_UPPER && z_circular_buffer_enqueue(&cache, &ptr))
+        return;
+
+    z_munmap(CRT_PTR_RAW(ptr), CRT_SIZE_ALLOC(ptr));
 }
 
 void *z_realloc(void *ptr, size_t size){
-    if (!size)
-        return NULL;
-
     if (ptr && size < CRT_SIZE_ALLOC(ptr) - CRT_SIZE_HDR) {
         CRT_SIZE_USER(ptr) = size;
         return ptr;
     }
 
-    size_t alloc_size = size + CRT_SIZE_HDR;
-
-    if (alloc_size % PAGE_SIZE)
-        alloc_size = ((alloc_size / PAGE_SIZE) + 1) * PAGE_SIZE;
-
-    void *memory = Z_RESULT_V(z_mmap(
-            NULL,
-            alloc_size,
-            PROT_READ | PROT_WRITE,
-            MAP_ANONYMOUS | MAP_PRIVATE,
-            -1,
-            0));
-
-    if (memory == MAP_FAILED) {
-        return NULL;
-    }
-
-    memory = CRT_PTR_USER(memory);
-
-    CRT_SIZE_USER(memory) = size;
-    CRT_SIZE_ALLOC(memory) = alloc_size;
+    void *p = z_malloc(size);
 
     if (ptr && CRT_SIZE_USER(ptr))
-        z_memcpy(memory, ptr, CRT_SIZE_USER(ptr));
+        z_memcpy(p, ptr, CRT_SIZE_USER(ptr) < size ? CRT_SIZE_USER(ptr) : size);
 
     if (ptr)
         z_free(ptr);
 
-    return memory;
+    return p;
 }
 
 void *z_malloc(size_t size) {
-    return z_realloc(NULL, size);
+    void *ptr = NULL;
+    size_t length = (size + CRT_SIZE_HDR + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1);
+
+    if (length <= HEAP_CACHE_UPPER) {
+        for (int i = 0; i < HEAP_CACHE_RETRY; i++, ptr = NULL) {
+            if (!z_circular_buffer_dequeue(&cache, &ptr))
+                break;
+
+            if (CRT_SIZE_ALLOC(ptr) >= length) {
+                CRT_SIZE_USER(ptr) = size;
+                break;
+            }
+
+            if (!z_circular_buffer_enqueue(&cache, &ptr)) {
+                z_free(ptr);
+            }
+        }
+    }
+
+    if (!ptr) {
+        void *memory = Z_RESULT_V(z_mmap(
+                NULL,
+                length,
+                PROT_READ | PROT_WRITE,
+                MAP_ANONYMOUS | MAP_PRIVATE,
+                -1,
+                0));
+
+        if (memory == MAP_FAILED) {
+            return NULL;
+        }
+
+        ptr = CRT_PTR_USER(memory);
+
+        CRT_SIZE_USER(ptr) = size;
+        CRT_SIZE_ALLOC(ptr) = length;
+    }
+
+    return ptr;
 }
 
 void *z_calloc(size_t num, size_t size) {
--- src/z_sync.c
+++ src/z_sync.c
@@ -1,6 +1,8 @@
 #include <z_sync.h>
 #include <z_log.h>
 #include <z_syscall.h>
+#include <z_memory.h>
+#include <z_std.h>
 #include <stdbool.h>
 #include <errno.h>
 #include <limits.h>
@@ -94,4 +96,73 @@ void z_rwlock_write_lock(z_rwlock_t *rwlock) {
 
 void z_rwlock_write_unlock(z_rwlock_t *rwlock) {
     z_mutex_unlock(&rwlock->w_mutex);
+}
+
+void z_circular_buffer_init(z_circular_buffer_t *buffer, unsigned long size, unsigned long length) {
+    buffer->head = 0;
+    buffer->tail = 0;
+    buffer->size = size;
+    buffer->length = length;
+
+    buffer->buffer = z_calloc(length, size);
+    buffer->state = z_calloc(length, sizeof(z_circular_buffer_state_t));
+}
+
+void z_circular_buffer_destroy(z_circular_buffer_t *buffer) {
+    z_free(buffer->buffer);
+    z_free(buffer->state);
+}
+
+unsigned long z_circular_buffer_size(z_circular_buffer_t *buffer) {
+    return (__atomic_load_n(&buffer->tail, __ATOMIC_SEQ_CST) % buffer->length + buffer->length - __atomic_load_n(&buffer->head, __ATOMIC_SEQ_CST) % buffer->length) % buffer->length;
+}
+
+bool z_circular_buffer_full(z_circular_buffer_t *buffer) {
+    return (__atomic_load_n(&buffer->tail, __ATOMIC_SEQ_CST) + 1) % buffer->length == __atomic_load_n(&buffer->head, __ATOMIC_SEQ_CST) % buffer->length;
+}
+
+bool z_circular_buffer_empty(z_circular_buffer_t *buffer) {
+    return __atomic_load_n(&buffer->head, __ATOMIC_SEQ_CST) == __atomic_load_n(&buffer->tail, __ATOMIC_SEQ_CST);
+}
+
+bool z_circular_buffer_enqueue(z_circular_buffer_t *buffer, const void *item) {
+    unsigned long index = __atomic_load_n(&buffer->tail, __ATOMIC_SEQ_CST);
+
+    do {
+        if (z_circular_buffer_full(buffer))
+            return false;
+    } while (!__atomic_compare_exchange_n(&buffer->tail, &index, (index + 1) % (ULONG_MAX - (ULONG_MAX % buffer->length)), true, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST));
+
+    index %= buffer->length;
+
+    while (!__sync_bool_compare_and_swap(&buffer->state[index], IDLE, PUTTING)) {
+
+    }
+
+    z_memcpy((char *)buffer->buffer + index * buffer->size, item, buffer->size);
+
+    __atomic_store_n(&buffer->state[index], VALID, __ATOMIC_SEQ_CST);
+
+    return true;
+}
+
+bool z_circular_buffer_dequeue(z_circular_buffer_t *buffer, void *item) {
+    unsigned long index = __atomic_load_n(&buffer->head, __ATOMIC_SEQ_CST);;
+
+    do {
+        if (z_circular_buffer_empty(buffer))
+            return false;
+    } while (!__atomic_compare_exchange_n(&buffer->head, &index, (index + 1) % (ULONG_MAX - (ULONG_MAX % buffer->length)), true, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST));
+
+    index %= buffer->length;
+
+    while (!__sync_bool_compare_and_swap(&buffer->state[index], VALID, TAKING)) {
+
+    }
+
+    z_memcpy(item, (char *)buffer->buffer + index * buffer->size, buffer->size);
+
+    __atomic_store_n(&buffer->state[index], IDLE, __ATOMIC_SEQ_CST);
+
+    return true;
 }
